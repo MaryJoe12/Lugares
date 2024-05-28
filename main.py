bd8f53e9b43112d0c1144d1abd43b2b17f08ded9
Maria Jose Medina Calderon <141747380+MaryJoe12@users.noreply.github.com>
2024-05-27 21:31:48 -0600

    Update main.py
    se resolvio los paises

--- main.py
+++ main.py
@@ -1,34 +1,47 @@
 import folium
 import requests
 import json
+import time
+#!pip install goslate
+#!pip install countryinfo
+from geopy import distance
+from countryinfo import CountryInfo
 coordinates ={}
 cordenadas=[]
 # Function to get the coordinates of a location using OpenStreetMap Nominatim API
+#segundo def
 def get_location_coordinates(location):
   url = f"https://nominatim.openstreetmap.org/search?q={location}&format=json"
   headers = {
     'User-Agent': 'Tryout',
-    'From': 'ingrese un correo'  # This is another valid field
+    'From': 'ingrese correo'  # This is another valid field
   }
-  response = requests.get(url).json()
+  response = requests.get(url,headers=headers).json()
   if len(response) == 0:
-    return None
+    lat = float(response[0]['lat'])
+    lon = float(response[0]['lon'])
+    print(lat, lon)
+    return (lat, lon)
   else:
       lat = float(response[0]['lat'])
       lon = float(response[0]['lon'])
       return (lat, lon)
+
 # Function to get the distance between two coordinates using OpenRouteService API
 def get_distance(origin, destination):
-  url = f"https://api.openrouteservice.org/v2/directions/driving-car?api_key="instert api"&start={origin[1]},{origin[0]}&end={destination[1]},{destination[0]}"
+  url = f"https://api.openrouteservice.org/v2/directions/driving-car?api_key=api key&start={origin[1]},{origin[0]}&end={destination[1]},{destination[0]}"
   headers = {'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8'}
   response = requests.get(url, headers=headers)
   if response.status_code == 200:
     data = response.json()
-    distance = data['features'][0]['properties']['segments'][0]['distance']
-    return distance
+    distance_2 = data['features'][0]['properties']['segments'][0]['distance']
+    return distance_2
   else:
-    return None
+    distance_2 = distance.distance(origin, destination)
+    return distance_2*1000
+
 # Main function to sort a list of places based on their proximity to a starting location
+#primer def
 def sort_places_by_proximity(starting_location, places_list):
   starting_coordinates = get_location_coordinates(starting_location)
   if starting_coordinates is None:
@@ -48,14 +61,19 @@ def sort_places_by_proximity(starting_location, places_list):
     distances.append((distance, place))
   sorted_places = [place for _, place in sorted(distances)]
   return sorted_places
+
 def lugares (sorted_places):
   for i in sorted_places:
     x = coordinates.get(i)
     cordenadas.append(x)
   return cordenadas
+
+#Start of the program
 starting_location = input("starting place ")
 places_input = input("Enter a comma-separated list of places: ")
+#Separate the places
 places_list = places_input.split(",")
+
 sorted_places = sort_places_by_proximity(starting_location, places_list)
 print("Sorted places:")
 for place in sorted_places:
